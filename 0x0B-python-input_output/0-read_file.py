2f2480e9f870f8cffd6995f3f6a29532f164abd4
Werku Birhanu Amsal <workzman12@gmail.com>
2022-09-28 12:55:56 +0300

    Python File IO Tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-'''Task 0 file IO'''
+"""Task 0 file IO"""
 
 
 def read_file(filename=""):
-    ''' A func which print the content of utf-8 encoding file'''
+    """ A func which print the content of utf-8 encoding file"""
     with open("my_file_0.txt", encoding="utf-8") as f:
         print(f.read(), end=" ")
