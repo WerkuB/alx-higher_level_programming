10b36c1673f32759c7f60acab44bb05d933b87c3
Werku Birhanu Amsal <workzman12@gmail.com>
2022-11-01 17:35:39 +0300

    SQL Introduction

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,3 @@
 -- a script which displays max temp by state
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state
+ORDER BY 1;
