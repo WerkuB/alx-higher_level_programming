8dd92cd53bd32b22e79c11f192a8f74b85884ea9
Werku Birhanu Amsal <workzman12@gmail.com>
2022-11-01 15:13:34 +0300

    SQL Introduction

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Create the second table and populate data
+CREATE TABLE IF NOT EXISTS second_table(
+	id int,
+	name varchar(256),
+	score  int
+	);
+INSERT second_table VALUES (1,'John',10);
+INSERT second_table VALUES (2,'Alex',3);
+INSERT second_table VALUES (3,'Bob',14);
+INSERT second_table VALUES (4,'George',8);
