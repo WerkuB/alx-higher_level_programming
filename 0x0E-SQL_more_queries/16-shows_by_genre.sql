8d76f4ad57d39bfa79bdc4561652d2a398c166d0
Werku Birhanu Amsal <workzman12@gmail.com>
2022-11-02 18:50:52 +0300

    More Queries

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,12 @@
+-- a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
