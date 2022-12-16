4fefb6509eb2658e307714940d7538052a1b4d87
Werku Birhanu Amsal <workzman12@gmail.com>
2022-12-16 04:08:15 -0800

    More on Python Networking

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 '''a Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email as 
+sends a POST request to the passed URL with the email as
 a parameter, and finally displays the body of the response.
 The email must be sent in the variable email
 You must use the packages requests and sys
