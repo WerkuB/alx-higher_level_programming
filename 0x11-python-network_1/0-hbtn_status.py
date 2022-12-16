585b9f658b313f82ca141449bd6f8962fe0f7136
Werku Birhanu Amsal <workzman12@gmail.com>
2022-12-16 03:21:20 -0800

    More on Python Networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 ''' A a Python script that fetches https://alx-intranet.hbtn.io/status
-
 You must use the package urllib
 You are not allowed to import any packages other than urllib
 You must use a with statement'''
 
 if __name__ == '__main__':
     import urllib.request
-
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
