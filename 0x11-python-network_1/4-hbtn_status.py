2a2f269b3b0f883b3a28c6ad1918491393562a30
Werku Birhanu Amsal <workzman12@gmail.com>
2022-12-16 04:51:26 -0800

    More on Python Networking

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,11 +2,11 @@
 '''a Python script that fetches https://alx-intranet.hbtn.io/status
 You must use the package requests
 You are not allow to import packages other than requests'''
+import requests
 
-if __name__ == '__main__':
-    import requests
 
-    res = requests.get('https://intranet.hbtn.io/status')
-    print('Body response:')
-    print('\t- type: {}'.format(type(res.text)))
-    print('\t- content: {}'.format(res.text))
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
